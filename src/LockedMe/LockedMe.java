49a0cd55dc554bafa45a4af1b9a22ce78e60d313
Avik Goon <avikgoon2020@gmail.com>
2022-01-23 03:14:57 +0530

    Created Wellcome Screen

---
+++ src/LockedMe/LockedMe.java
@@ -0,0 +1,18 @@
+package LockedMe;
+
+public class LockedMe {
+	public static void main(String args []) {
+		System.out.println("*****************************************");
+		System.out.println("*\t\t\t\t\t*");	
+		System.out.println("*\t\t\t\t\t*");	
+		System.out.println("*\t\t\t\t\t*");	
+		System.out.println("*\t ------WELCOME TO------\t\t*");	
+		System.out.println("*\t\tLOCKEDME\t\t*");				
+		System.out.println("*\t\t\t\t\t*");	
+		System.out.println("*\t\t\t\t\t*");	
+		System.out.println("*****************************************");
+		
+		System.out.println("💻Developer Name: Avik Goon");
+		System.out.println("📪️️Developer Email: goonavik@lockedme.com");
+	}
+}
