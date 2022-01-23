4094b8713671dff74a03af060217aa217672d4d3
Avik Goon <avikgoon2020@gmail.com>
2022-01-23 23:00:01 +0530

    final commit

--- src/LockedMe/FileOperations.java
+++ src/LockedMe/FileOperations.java
@@ -6,7 +6,8 @@ package LockedMe;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author avikgoon
@@ -14,7 +15,9 @@ import java.util.ArrayList;
  */
 public class FileOperations {
 	String fileName;
-	public void printFileNames(File[] a, int i, int lvl, ArrayList<String> fileNames) {
+
+	public void printFileNames(File[] a, int i, int lvl, List<String> fileNames){
+		
 		// base case of the recursion
 		// i == a.length means the directory has
 		// no more files. Hence, the recursion has to stop
@@ -25,26 +28,26 @@ public class FileOperations {
 		if (a[i].isFile()) {
 			String fileName = a[i].getName();
 			fileNames.add(fileName);
-			//System.out.println(fileName);
+			// System.out.println(fileName);
 		}
 		// recursively printing files from the directory
 		// i + 1 means look for the next file
 		printFileNames(a, i + 1, lvl, fileNames);
 	}
-	
+
 	public void getFileName(BufferedReader inp) {
 		System.out.println("Enter a file name: ");
 		try {
 			this.fileName = inp.readLine();
-			
+
 		} catch (Exception e) {
 			System.err.println(e.getMessage());
 		}
-		
+
 	}
 
-	public void createNewFile(BufferedReader inp, String path) throws IOException {
-		
+	public void createNewFile(BufferedReader inp, String path) throws IOException, InterruptedException {
+
 		getFileName(inp);
 		try {
 			String fullPath = path + "/" + this.fileName;
@@ -57,14 +60,49 @@ public class FileOperations {
 		} catch (Exception e) {
 			System.err.println(e.getLocalizedMessage());
 		} finally {
+			TimeUnit.SECONDS.sleep(1);
+			System.out.println("\n\nPress enter/return key to continue...");
+			System.in.read();
+		}
+
+	}
+
+	public void deleteFile(BufferedReader inp, String path) throws IOException {
+		getFileName(inp);
+		String fullPath = path + "/" + this.fileName;
+		try {
+			File fileToBeDeleted = new File(fullPath);
+			if (fileToBeDeleted.delete()) {
+				System.out.println("File deleted successfully");
+			} else
+				throw new Exception("File Not Found...🔎");
+		} catch (Exception e) {
+			System.err.println(e.getLocalizedMessage());
+		} finally {
 			System.out.println("\n\nPress enter/return key to continue...");
 			System.in.read();
 		}
 
 	}
 	
-	public void deleteFile(BufferedReader inp, String path) throws IOException {
-		
+	public void doFileExist( BufferedReader inp, String path) throws IOException, InterruptedException{
+		getFileName(inp);
+		String fullPath = path + "/" + this.fileName;
+		System.out.println( "\n👀 Serching..Please Wait....... \n" );
+		TimeUnit.SECONDS.sleep(2);
+		try {
+			File file = new File(fullPath);
+			if ( file.exists() ) {
+				System.out.println("File found in system..");
+			} else
+				throw new Exception("File Not Found...🔎");
+		} catch (Exception e) {
+			System.err.println(e.getLocalizedMessage());
+		} finally {
+			TimeUnit.SECONDS.sleep(1);
+			System.out.println("\n\nPress enter/return key to continue...");
+			System.in.read();
+		}
 	}
 
 }
--- src/LockedMe/LockedMe.java
+++ src/LockedMe/LockedMe.java
@@ -3,76 +3,94 @@ package LockedMe;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 public class LockedMe {
-	public static void main(String args []) throws IOException {
+	public static void main(String args[]) throws IOException {
 		Scanner input = new Scanner(System.in);
-		BufferedReader inp = new BufferedReader (new InputStreamReader(System.in));
+		BufferedReader inp = new BufferedReader(new InputStreamReader(System.in));
 		String path = "././LockedMeLib";
 		File fileObj = new File(path);
 		FileOperations fileoperations = new FileOperations();
 		int option = 0;
-		//display welcome message
+		// display welcome message
 		new WelcomeMessage().showWellcomeGraphic();
-		
-		//ask user good name, then proceed
-		String username = (String) new UserDetails(input).getUserName();		
-		System.out.println("Hi,🙋 "+username);
-		
-		mainmenu: do {	
+
+		// ask user good name, then proceed
+		String username = (String) new UserDetails(input).getUserName();
+		System.out.println("Hi,🙋 " + username);
+
+		mainmenu: do {
 			try {
-			System.out.println("Choose any option from the following: 👇");
-			System.out.println("1.👀 Show all Files");
-			System.out.println("2.➡️ More File Operations");
-			System.out.println("3.🚪 Exit");
-			option = input.nextInt();
-			switch (option) {
+				System.out.println("Choose any option from the following: 👇");
+				System.out.println("1.👀 Show all Files");
+				System.out.println("2.➡️ More File Operations");
+				System.out.println("3.🚪 Exit");
+				option = input.nextInt();
+				switch (option) {
 				case 1:
-					if( fileObj.exists() && fileObj.isDirectory()) {
-						ArrayList<String> fileNames = new ArrayList<String>();
+					// create new file
+					if (fileObj.exists() && fileObj.isDirectory()) {
+						final List<String> fileNames = new ArrayList<String>();
+
 						File[] listofFiles = fileObj.listFiles();
+						System.out.println( "\n⏳ Loading..Please Wait....... \n" );
+						TimeUnit.SECONDS.sleep(2);
 						System.out.println("\n---- 🗂 ----\n");
+
 						fileoperations.printFileNames(listofFiles, 0, 0, fileNames);
-						Collections.sort(fileNames);
-						String listOfFiles = String.join(",\n", fileNames);
+
+						// convert all file name to lower case
+						List<String> result = fileNames.stream().map(String::toLowerCase).collect(Collectors.toList());
+						// sort file name in ascending order
+						Collections.sort(result);
+						// convert list to string
+						String listOfFiles = String.join(",\n", result);
+
 						System.out.println(listOfFiles);
 						System.out.println("\n\nThat's it...\nPress enter/return key to continue...");
 						System.in.read();
-					}else throw new FileNotFoundException("The path you given is invalid");
+					} else
+						throw new FileNotFoundException("The path you given is invalid");
 					break;
-				
+
 				case 2:
 					int submenuOptionNumber = new MoreOptions(input).createMoreFileOptions();
-					submenu: while ( submenuOptionNumber > 0 ) {
-						if( submenuOptionNumber == 1 ) {
+					submenu: while (submenuOptionNumber > 0) {
+						if (submenuOptionNumber == 1) {
 							// create a new file
-							fileoperations.createNewFile( inp, path );
-							
-							//calling sub-menu again
+							fileoperations.createNewFile(inp, path);
+
+							// calling sub-menu again
 							submenuOptionNumber = new MoreOptions(input).createMoreFileOptions();
 							continue submenu;
-						}else if( submenuOptionNumber == 2 ) {
+						} else if (submenuOptionNumber == 2) {
 							// delete a existing file
 							fileoperations.deleteFile(inp, path);
-							//calling sub-menu again
+							// calling sub-menu again
 							submenuOptionNumber = new MoreOptions(input).createMoreFileOptions();
 							continue submenu;
-						} else if( submenuOptionNumber == 3 ) {
+						} else if (submenuOptionNumber == 3) {
 							// search for a file
-							
-							//calling sub-menu again
+							fileoperations.doFileExist(inp, path);
+							// calling sub-menu again
 							submenuOptionNumber = new MoreOptions(input).createMoreFileOptions();
 							continue submenu;
-						} else continue mainmenu;  //going back to main menu
-					} 
+						} else
+							continue mainmenu; // going back to main menu
+					}
 					break;
 				case 3:
-					input.close();System.out.println("Good Bye ..🙋");
+					input.close();
+					System.out.println("Good Bye ..🙋");
 					System.exit(0);
-				
+
 				default:
-					throw new IllegalArgumentException("Unexpected value: " + option+"\nProgram will terminate now...");
+					throw new IllegalArgumentException(
+							"Unexpected value: " + option + "\nProgram will terminate now...");
 				}
 			} catch (Exception e) {
 				System.err.println(e.getLocalizedMessage());
@@ -80,8 +98,8 @@ public class LockedMe {
 				input.close();
 				System.exit(0);
 			}
-			
-		}while(true);
-		
+
+		} while (true);
+
 	}
 }
